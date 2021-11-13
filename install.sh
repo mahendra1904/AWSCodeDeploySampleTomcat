1095355409811e2bac928120e6b70628931f4d4d
MAHENDRA KUMAR MISHRA <86907255+mahendra1904@users.noreply.github.com>
2021-11-13 14:56:30 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-aws s3 cp s3://java-artifacts-devops4solutions/target/LoginWebApp-1.war/sample-java-projects/LoginWebApp-1.war /tmp
+aws s3 cp s3://kaholo-web-app-bucket-1/kaholo-app-build/LoginWebApp-1.war /tmp
 sudo mv /tmp/LoginWebApp-1.war /usr/share/tomcat/webapps/LoginWebApp-1.war
 sudo service tomcat restart
