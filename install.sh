f67b82660d99596ee9f0c470978f0a1473f4a247
MAHENDRA KUMAR MISHRA <86907255+mahendra1904@users.noreply.github.com>
2021-11-16 12:07:30 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
-aws s3 cp s3://java-app-kaholo-tomcat/java-app /tmp
-sudo mv /tmp /usr/share/tomcat/webapps/
+aws s3 cp s3://java-app-kaholo-tomcat/test-build/LoginWebApp-1.war /tmp
+
+
+sudo mv /tmp /usr/share/tomcat/webapps
 sudo service tomcat restart
